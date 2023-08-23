03ccd0008c8a82e24adc03dfa0825c827744b053
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-08-23 23:35:52 +0200

    init

---
+++ main.js
@@ -0,0 +1,5 @@
+
+import image from './src/assets/images/01.png'
+
+console.log(image)
+document.getElementById('images').src = image;
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+// vite.config.ts
+import Inspect from 'vite-plugin-inspect'
+
+export default {
+  base: "/filmoteka",
+  plugins: [
+    Inspect()
+  ],
+}
